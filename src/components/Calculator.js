33c06677e425b97c7cc3acc020f56c5ae8b57352
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-07-02 00:30:07 -0700

    can now calcute 2/5 price and shares amount

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import NumericInput from 'react-numeric-input';
 
 const Calculator = () => {  
-    const handleSubmit = () => {
-        console.log(`calculator.js - 5 - ✅`);
-    }
+    // store investment amount and share price values to state
+    const [investmentAmount, setInvestmentAmount] = useState(0);
+    const [shares, setShares] = useState(0);
 
-    const handleChange = () => {
-        console.log(`calculator.js - 9 - 🌦`);
-    }
+    // store 2/5 price and share state
+    const [twoFifthsPrice, setTwoFifthsPrice] = useState(0)
+    const [twoFifthsShares, setTwoFifthsShares] = useState(0)
+
+    useEffect(() => {
+        console.log(`calculator.js - 9 - 🏝`, {investmentAmount, shares});
+        
+        // calculate twoFifthsPrice
+        setTwoFifthsPrice(investmentAmount * 0.4)
+        // calculate twoFifthsShares
+        if (investmentAmount === 0 && shares === 0) {
+            setTwoFifthsShares(0)
+        } 
+        if(shares === 0) {
+
+        } else {
+            setTwoFifthsShares(investmentAmount/shares)
+        }
+    }, [investmentAmount, shares]);
+    
+    // const handleSubmit = () => {
+    //     console.log(`calculator.js - 5 - ✅`);
+    // }
+
+    // const handleChange = () => {
+    //     console.log(`calculator.js - 9 - 🌦`);
+    // }
 
     return (
       <div className="cal-bg">
@@ -19,15 +44,11 @@ const Calculator = () => {
             <div className="row-two">
                 <div>
                     <div className="subtitle">Investment Amount</div>
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" value="" onChange={handleChange} />
-                    </form>
+                    <NumericInput value={investmentAmount} onChange={setInvestmentAmount}/>
                 </div>
                 <div>
                     <div className="subtitle">Price per share</div>
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" value="" onChange={handleChange} />
-                    </form>
+                    <NumericInput step={0.1} precision={4} value={shares} onChange={setShares}/>
                 </div>
             </div>
             <div className="row-three">
@@ -36,11 +57,11 @@ const Calculator = () => {
                     <div>
                         <div className="init">
                             <div className="subtitle-light">Initial share price</div>
-                            <div className="amount">200</div>
+                            <div className="amount">{twoFifthsPrice}</div>
                         </div>
                         <div className="init">
                             <div className="subtitle-light">Initial shares</div>
-                            <div className="shares">100</div>
+                            <div className="shares">{twoFifthsShares}</div>
                         </div>
                     </div>
                 </div>
